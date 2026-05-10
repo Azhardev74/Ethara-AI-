75ff3273a4acebf3117daf50ab7c0b3a86a5cdf1
azhar-dev <azhar.webdev1@gmail.com>
2026-05-10 16:22:24 +0530

    final

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "ethara-ai-production-e2dc.up.railway.app/api"
+  baseURL: import.meta.env.VITE_API_URL || "https://ethara-ai-yk2g.onrender.com/api"
 });
 
 api.interceptors.request.use((config) => {
